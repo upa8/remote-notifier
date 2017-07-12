c981dd060bfbbe35df3d5139f40ccb8788103de6
Ashish Deshpande <ashishd751@gmail.com>
2017-07-12 15:04:16 +0530

    Changed api

--- index.js
+++ index.js
@@ -11,71 +11,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/saveUser', function (req, res) {
-   console.log(req.body);
+   console.log( 'Token '+req.body.token);
 
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
        data = JSON.parse( data );
        data.users.push(req.body);
        console.log( data );
    });
+
    res.status(200).send({status: 'SUCCESS'});
-})
 
-
-app.get('/listUsers', function (req, res) {
-  //  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-  //      console.log( data );
-  //      res.end( data );
-  //  });
-
-  // To check if something is a push token
-  let isPushToken = Expo.isExponentPushToken('ExponentPushToken[1CasSGA3kQGHdsRnkA6Wta]');
+   console.log('aFTER Notification')
+    // To check if something is a push token
+  let isPushToken = Expo.isExponentPushToken(req.body.token);
 
   // Create a new Expo SDK client
   let expo = new Expo();
 
   // To send push notifications -- note that there is a limit on the number of
   // notifications you can send at once, use expo.chunkPushNotifications()
-
     try {
-      let receipts = expo.sendPushNotificationsAsync([{
-        // The push token for the app user to whom you want to send the notification
-        to: 'ExponentPushToken[1CasSGA3kQGHdsRnkA6Wta]',
-        sound: 'default',
-        body: 'My first remote notification',
-        data: {withSome: 'Office hours completed!'},
-      }]);
-      console.log(receipts);
-      res.end('Notification sent !');
+    	console.log('Inside notifications' + req.body.token);
+	    let receipts = await expo.sendPushNotificationsAsync([{
+	        // The push token for the app user to whom you want to send the notification
+	        to: req.body.token,
+	        sound: 'default',
+	        body: 'Welcome to our app',
+	        data: {withSome: 'data'},
+	      }]);
+      console.log('SUCCESS '+receipts);
     } catch (error) {
-      console.error(error);
+      console.error('eRROR '+error);
     }
-
-})
-
-app.get('/:id', function (req, res) {
-  // To check if something is a push token
-  let isPushToken = Expo.isExponentPushToken(somePushToken);
-
-  // Create a new Expo SDK client
-  let expo = new Expo();
-
-  // To send push notifications -- note that there is a limit on the number of
-  // notifications you can send at once, use expo.chunkPushNotifications()
-  (async function() {
-    try {
-      let receipts = await expo.sendPushNotificationsAsync([{
-        // The push token for the app user to whom you want to send the notification
-        to: 'ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]',
-        sound: 'default',
-        body: 'This is a test notification',
-        data: {withSome: 'data'},
-      }]);
-      console.log(receipts);
-    } catch (error) {
-      console.error(error);
-    }
-  })();
+  
 })
 
 app.get('/test', function (req, res) {
